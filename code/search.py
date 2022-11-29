177809486cad89559b2d03c2c0c0298e8fd370fc
orihiroo <yeyuma2@illinois.edu>
2022-11-28 23:07:51 -0500

    update search.py
    Update search.py

--- code/search.py
+++ code/search.py
@@ -19,7 +19,6 @@ class Query():
         self.tfs = {}                           # permanent storage for tfs of all tokens in all documents
         self.lengths = Counter()                # used for calculating lengths of documents
         self.postings_list = {}                 # posting list storage for each token in the corpus
-        self.st_tokens = []
 
     def read_csv(self):
         with open(self.corpusroot, 'r', encoding = "ISO-8859-1") as read_obj:
@@ -38,14 +37,14 @@ class Query():
             print("needed_info", needed_info)
             print("==============")
 
+    def get_idf(self, token):
+        if self.df[token] == 0:
+            return -1
+        return log10(len(self.tfs) / self.df[token])  # len(tfs) returns no. of docs; df[token] returns the token's document frequency
+
     def cal_weight(self, text, token):                                      #returns the weight of a token in a document without normalizing
         self.idf = self.get_idf(token)
         return (1+log10(self.tfs[text][token]))*self.idf                       #tfs has the logs of term frequencies of all docs in a multi-level dict
-
-    def get_idf(self, token):
-        if self.df[token]==0:
-            return -1
-        return log10(len(self.tfs)/self.df[token])                #len(tfs) returns no. of docs; df[token] returns the token's document frequency
 
     def cal_tf_idf(self):
         for text in self.tfs:                                #loop for calculating tf-idf vectors and lengths of documents
@@ -53,8 +52,8 @@ class Query():
             length=0
             for token in self.tfs[text]:
                 weight = self.cal_weight(text, token)         #calWeight calculates the weight of a token in a doc without normalization
-                self.vectors[text][token]=weight             #this is the weight of a token in a file
-                length += weight**2                         #calculating length for normalizing later
+                self.vectors[text][token]=weight             #this is the weight of a token in a text doc
+                length += weight**2
             self.lengths[text]=math.sqrt(length)
 
     def norm_weight(self):
@@ -65,7 +64,7 @@ class Query():
                     self.postings_list[token]=Counter()
                 self.postings_list[token][text] = self.vectors[text][token]
 
-    def query(self,qstring):                                        #function that returns the best match for a query
+    def query(self,qstring):
         qstring=qstring.lower()
         qtf={}
         qlength=0
@@ -80,11 +79,11 @@ class Query():
             if self.get_idf(token)==0:                    #if a token has idf = 0, all values in its postings list are zero.
                 loc_docs[token], weights = zip(*self.postings_list[token].most_common())         #to avoid that, we store all docs
             else:
-                loc_docs[token],weights = zip(*self.postings_list[token].most_common(2))         #taking top 2 in postings list, NEED to increase this number once have more data
+                loc_docs[token],weights = zip(*self.postings_list[token].most_common(2))         #taking top 2 in postings list, NEED to increase this number once have more data!!
             if len(weights) == 1:
                 tenth[token] = weights[0]
             else:
-                tenth[token] = weights[1]                                                         #storing the upper bound of each token. NEED to increase this number once have more data
+                tenth[token] = weights[1]                                                         #storing the upper bound of each token. NEED to increase this number once have more data!!
             if flag==1:
                 commondocs=set(loc_docs[token]) & commondocs                                #commondocs keeps track of docs that have all tokens
             else:
@@ -101,10 +100,10 @@ class Query():
                 else:
                     cos_sim = 0
             cos_sims[doc]=cos_sim
-        max=cos_sims.most_common(1)                                                              #seeing which doc has the max value
+        max=cos_sims.most_common(1)                                                              #seeing which doc/subjecct id has the max value
         ans,wght=zip(*max)
         try:
-            if ans[0] in commondocs:                                                             #if doc has actual score, return score
+            if ans[0] in commondocs:                                                             #if text doc has actual score, return score
                 return ans[0],wght[0]
             else:
                 return "fetch more",0                                                            #if upperbound score is greater, return fetch more
@@ -118,6 +117,7 @@ https://stackoverflow.com/questions/18424228/cosine-similarity-between-2-number-
 https://stackoverflow.com/questions/15890503/valueerror-math-domain-error
 https://thispointer.com/python-read-a-csv-file-line-by-line-with-or-without-header/
 https://www.nltk.org/api/nltk.tokenize.html
+https://towardsdatascience.com/finding-word-similarity-using-tf-idf-in-a-term-context-matrix-from-scratch-in-python-e423533a407
 """
 if __name__ == '__main__':
     query1 = Query()
